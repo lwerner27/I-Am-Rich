cc9585e78e2de4b39b0e9a502ba2983fc6f86f12
Lucas Werner <lwerner27@gmail.com>
2018-11-15 10:45:56 -0600

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Lucas Werner on 11/15/18.
+//  Copyright © 2018 Lucas Werner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
